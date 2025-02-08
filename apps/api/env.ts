c3876f53b0ca69d25445f68e81072add14a9e199
Ayush Kumar Yadav <69889382+zaCKoZAck0@users.noreply.github.com>
2025-02-08 07:49:50 +0000

    init: everything works

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -3,18 +3,18 @@ import { keys as auth } from "@repo/auth/keys";
 import { keys as database } from "@repo/database/keys";
 import { keys as email } from "@repo/email/keys";
 import { keys as core } from "@repo/next-config/keys";
-// import { keys as observability } from "@repo/observability/keys";
+import { keys as observability } from "@repo/observability/keys";
 // import { keys as payments } from "@repo/payments/keys";
 import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   extends: [
-    // auth(),
+    auth(),
     // analytics(),
-    // core(),
-    // database(),
-    // email(),
-    // observability(),
+    core(),
+    database(),
+    email(),
+    observability(),
     // payments(),
   ],
   server: {},
